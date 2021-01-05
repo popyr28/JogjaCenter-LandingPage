9e33e8817177a67cdffb637ddca8fc64b4b2d1f6
popyr28 <phipo.andini@gmail.com>
2021-01-05 22:55:22 +0700

    update

---
+++ frontend/scripts/main.js
@@ -0,0 +1,16 @@
+const dimensiSlide = document.querySelector('.group-slide');
+const dimensiImage = document.querySelectorAll('.group-slide');
+
+const btnPrev = document.querySelector('#btnPrev');
+const btnNext = document.querySelector('#btnNext');
+
+let counter = 1;
+const size = dimensiImage[0].clientWidth + dimensiImage[1].clientWidth;
+
+dimensiSlide.style.transform = 'translateX(' +  (-size * counter) + 'px)';
+
+btnNext.addEventListener('click', ()=>{
+    dimensiSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    console.log(counter)
+})
