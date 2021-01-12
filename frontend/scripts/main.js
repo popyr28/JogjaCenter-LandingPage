1ff11dfd426de54e5cfd38c5a6c25c0087a11fd3
popyr28 <phipo.andini@gmail.com>
2021-01-12 20:46:20 +0700

    update

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,12 +1,26 @@
-$('.page-scroll').on('click', function(e){
-    var dest = $(this).attr('href');
+// $('.page-scroll').on('click', function(e){
+//     var dest = $(this).attr('href');
     
-    var elementDest = (dest);
-    console.log(elementDest);
+//     var elementDest = (dest);
+//     console.log($('body').scrollTop);
 
-    $('body').animate({
-        scrollTop: elementDest.offset().top
+//     $('body').animate({
+//         scrollTop: elementDest.offset().top
+//     });
+
+//     e.preventDefault();
+// });
+
+
+jQuery( document ).ready( function($) {
+    var elementAbout = $('#navbar');
+    console.log(elementAbout.offset());
+    $( window ).scroll( function () {
+        console.log($('body').scroll());
+    //   if ( $('body').scrollTop() > 0 ) {
+    //     $('body').animate({
+    //         scrollTop: elementAbout.offset().top
+    //     });
+    // }
     });
-
-    e.preventDefault();
-});
+  });
